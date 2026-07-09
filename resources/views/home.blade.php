0aa560f7cc06d7b80bc1a19b6d5dd5cc46444132
andyyoung <jamesandrewpurvis@gmail.com>
2026-07-09 16:49:45 -0500

    finished skills section.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -64,4 +64,118 @@
     <div class="page-header">
         <h3>Skills</h3>
     </div>
+
+        <div class="skills-row">
+            <div class="skills-card">
+                <div class="skills-circle">
+                    <i data-lucide="code" class="skill-icon"></i>
+                </div>
+                <div class="skill-heading">
+                    <h4>Programming</h4>
+                </div>
+                <div class="skill-list">
+                    <div class="skills-item">
+                        <i data-lucide="computer" class="skills-tiny-icon"></i>
+                        <p>Java</p>
+                    </div>
+                    <div class="skills-item">
+                        <i data-lucide="shield-half" class="skills-tiny-icon"></i>
+                        <p>C#</p>
+                    </div>
+                    <div class="skills-item">
+                        <i data-lucide="database" class="skills-tiny-icon"></i>
+                        <p>Relational Databases</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="arrow-up-from-line" class="skills-tiny-icon"></i>
+                        <p>Git/GitHub</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="heading-1" class="skills-tiny-icon"></i>
+                        <p>Basic HTML</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="cpu" class="skills-tiny-icon"></i>
+                        <p>Basic CSS</p>
+                    </div>
+                </div>
+            </div>
+            <div class="skills-card">
+                <div class="skills-circle">
+                    <i data-lucide="calculator" class="skill-icon"></i>
+                </div>
+                <div class="skill-heading">
+                    <h4>Accounting</h4>
+                </div>
+                <div class="skill-list">
+                    <div class="skills-item">
+                        <i data-lucide="square-function" class="skills-tiny-icon"></i>
+                        <p>Financial Accounting</p>
+                    </div>
+                    <div class="skills-item">
+                        <i data-lucide="folder-kanban" class="skills-tiny-icon"></i>
+                        <p>Managerial Accounting</p>
+                    </div>
+                    <div class="skills-item">
+                        <i data-lucide="circle-dollar-sign" class="skills-tiny-icon"></i>
+                        <p>Taxation</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="shield" class="skills-tiny-icon"></i>
+                        <p>Auditing</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="sheet" class="skills-tiny-icon"></i>
+                        <p>Microsoft Excel</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="flag" class="skills-tiny-icon"></i>
+                        <p>Financial Reporting</p>
+                    </div>
+                </div>
+            </div>
+            <div class="skills-card">
+                <div class="skills-circle">
+                    <i data-lucide="code" class="skill-icon"></i>
+                </div>
+                <div class="skill-heading">
+                    <h4>Other Skills</h4>
+                </div>
+                <div class="skill-list">
+                    <div class="skills-item">
+                        <i data-lucide="book-user" class="skills-tiny-icon"></i>
+                        <p>Attention to Detail</p>
+                    </div>
+                    <div class="skills-item">
+                        <i data-lucide="info" class="skills-tiny-icon"></i>
+                        <p>Problem Solving</p>
+                    </div>
+                    <div class="skills-item">
+                        <i data-lucide="smile" class="skills-tiny-icon"></i>
+                        <p>Self-Motivation</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="podcast" class="skills-tiny-icon"></i>
+                        <p>Communication</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="brain" class="skills-tiny-icon"></i>
+                        <p>Critical Thinking</p>
+                    </div>
+
+                    <div class="skills-item">
+                        <i data-lucide="clock" class="skills-tiny-icon"></i>
+                        <p>Time Management</p>
+                    </div>
+                </div>
+            </div>
+        </div>
 @endsection
